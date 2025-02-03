b22eebc4985d199a9d97b1f7ac0193b023641b07
hectoraria <145675493+hectoraria@users.noreply.github.com>
2025-02-03 14:36:05 +0100

    clsUsuario END

---
+++ ENT/clsUsuario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class clsUsuario
+    {
+        #region Atributos
+
+        private int id;
+
+        private string nombre;
+
+        private int score;
+
+        #endregion
+
+        #region Propiedades
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Nombre
+        {
+
+            get { return nombre; }
+        }
+
+        public int Score
+        {
+            get { return score; }
+        }
+
+        #endregion
+
+        #region Contructores
+
+        public clsUsuario(string nombre)
+        {
+            this.nombre=nombre;
+        }
+
+        #endregion
+    }
+}
