6a4d65bd2c666cd6000ba79a623202ec1ccb384d
hectoraria <145675493+hectoraria@users.noreply.github.com>
2025-02-12 14:35:20 +0100

    VMRegister casi, VMWanted empezado

---
+++ LuigiWanted/VM/pantallaWantedVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENT;
+
+
+namespace LuigiWanted.VM
+{
+    public class pantallaWantedVM
+    {
+        #region Atributos
+
+        private int tiempoSiguienteRonda;
+        private List<clsUsuario> listaPuntuacion;
+
+        #endregion
+
+        #region Propiedades
+
+        public int TiempoSiguienteRonda
+        {
+            get { return tiempoSiguienteRonda; }
+            set { tiempoSiguienteRonda = value; }
+        }
+
+        public List<clsUsuario> ListaPuntuacion
+        {
+            get { return listaPuntuacion; }
+            set { listaPuntuacion = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public pantallaWantedVM()
+        {
+            listaPuntuacion = new List<clsUsuario>();
+        }
+
+        #endregion
+
+
+
+
+    }
+}
