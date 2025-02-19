9e4062910dc53915acdf081ea35eb9f175353f7a
eduarnesto <145675429+eduarnesto@users.noreply.github.com>
2025-02-19 14:04:39 +0100

    Creacion del Hub

--- LuigiWantedSignalR/LuigiWantedSignalR/Hubs/GameHub.cs
+++ LuigiWantedSignalR/LuigiWantedSignalR/Hubs/GameHub.cs
@@ -8,6 +8,7 @@ public class GameHub : Hub
 {
     private static List<clsUsuario> listaUsuarios = new List<clsUsuario>();
 
+<<<<<<< Updated upstream
     public static List<clsUsuario> ListaUsuarios()
     {
         return listaUsuarios;
@@ -25,6 +26,19 @@ public class GameHub : Hub
     {
         clsPersonaje personaje = clsListadosPersonajeBL.GetPersonajeAleatorio();
         await Clients.All.SendAsync("PersonajeBuscado", personaje);
+=======
+    public async Task Registrarse(string nombre)
+    {
+        clsUsuario usuario = clsListadosUsuarioBL.addUserBL(nombre);
+        listaUsuarios.Add(usuario);
+        await Clients.All.SendAsync("UsuarioUnido", nombre);
+    }
+
+    public async Task EmpezarPantallaWanted()
+    {
+        clsPersonaje personaje = clsListadoPersonajeBL.ObtenerPersonajeAleatorio();
+        await Clients.All.SendAsync("EmpezarPantallaWanted", personaje);
+>>>>>>> Stashed changes
     }
 
     public async Task PersonajeEncontrado(string nombre)
@@ -32,14 +46,24 @@ public class GameHub : Hub
         await Clients.All.SendAsync("PersonajeEncontrado", nombre);
     }
 
+<<<<<<< Updated upstream
     public async Task ScoreUpdate(int score)
     {
         // Lógica para actualizar el puntaje
         await Clients.All.SendAsync("ScoreActualizado", score);
+=======
+    public async Task ScoreUpdate()
+    {
+        await Clients.All.SendAsync("ScoreActualizado");
+>>>>>>> Stashed changes
     }
 
     public async Task EmpezarJuego()
     {
+<<<<<<< Updated upstream
         await Clients.All.SendAsync("JuegoListo", ListaUsuarios().Count);
+=======
+        await Clients.All.SendAsync("JuegoListo", listaUsuarios.Count);
+>>>>>>> Stashed changes
     }
 }
